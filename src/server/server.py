45939eccb5d1f6eec41254b63133da298625b2e7
arcticlimer <vinigm.nho@gmail.com>
2020-10-29 17:30:28 -0300

    🗃 Added database instance into the server class

--- src/server/server.py
+++ src/server/server.py
@@ -3,23 +3,36 @@ from typing import Tuple
 from socketserver import ThreadingMixIn
 from socketserver import TCPServer
 
+from server.request_handler import BaseHTTPRequestHandler
+from database.database import Database
+
 
 class PasteServer(TCPServer, ThreadingMixIn):
-    """Server class that inherits ThreadingMixin for multithread"""
+    """
+    Server class that inherits ThreadingMixin for multithread
+    
+    Server class constructor.
+
+    Parameters
+    ----------
+    address : Tuple[`str`, `int`]
+        Tuple containing the server IP address (e.g "127.0.0.1")
+        and an int representing the server port (e.g 8080).
+
+    handler :
+        A request handling class supported by 
+        the `ThreadingHTTPServer` class.
+    
+    db : `Database`
+        Database instance that will be managed by the
+        `RequestHandlerClass` instances.
+    """
+
     def __init__(self, *,
                  server_address: Tuple[str, int],
-                 RequestHandlerClass):
-        """
-        Server class constructor.
+                 RequestHandlerClass: BaseHTTPRequestHandler,
+                 db: Database):
 
-        Parameters
-        ----------
-        address : Tuple[`str`, `int`]
-            Tuple containing the server IP address (e.g "127.0.0.1")
-            and an int representing the server port (e.g 8080).
-
-        handler :
-            A request handling class supported by 
-            the `ThreadingHTTPServer` class.
-        """
         super().__init__(server_address, RequestHandlerClass)
+        self.db = db
+      
